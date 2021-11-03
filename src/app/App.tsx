b725cc19c63d1bab3d47e971a13578304186079a
Anke <pfeiffer.anke@outlook.com>
2021-11-03 12:16:09 +0100

    :sparkles: Add welcome page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import styles from './App.module.css';
 
 function App(): JSX.Element {
-  return <main>Hello 🐬</main>;
+  return (
+    <body className={styles.container}>
+      <h1 className={styles.heading}>Bergfest</h1>
+      <form className={styles.form}>
+        <input
+          type="text"
+          className={styles.inputField}
+          placeholder="first name"
+        ></input>
+        <input
+          type="text"
+          className={styles.inputField}
+          placeholder="last name"
+        ></input>
+        <input type="submit" className={styles.button}></input>
+      </form>
+    </body>
+  );
 }
 
 export default App;
