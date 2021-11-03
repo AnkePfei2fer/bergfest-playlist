9633879fdda22788e94ae1a1e25ccc86ef74e2e8
Anke <pfeiffer.anke@outlook.com>
2021-11-03 13:00:23 +0100

    :recycle: Separate Title component, improve styling, insert participant selection

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import styles from './App.module.css';
+import Title from './components/Title/Title';
 
 function App(): JSX.Element {
   return (
-    <body className={styles.container}>
-      <h1 className={styles.heading}>Bergfest</h1>
+    <main className={styles.container}>
+      <Title />
       <form className={styles.form}>
+        <span className={styles.options}>select participant</span>
+        <select className={styles.selection}>
+          <option>Leon Machens</option>
+          <option>Lara Janzen</option>
+        </select>
+        <span className={styles.options}>or create new</span>
         <input
           type="text"
           className={styles.inputField}
@@ -16,9 +23,9 @@ function App(): JSX.Element {
           className={styles.inputField}
           placeholder="last name"
         ></input>
-        <input type="submit" className={styles.button}></input>
+        <input type="submit" className={styles.button} value={'Enter'}></input>
       </form>
-    </body>
+    </main>
   );
 }
 
