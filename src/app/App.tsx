c2d29f822d36a4787bf91a11188d960bc14e1e4e
Anke <pfeiffer.anke@outlook.com>
2021-11-03 16:58:50 +0100

    :recycle: Migrate form element to separate file

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,30 +1,13 @@
 import React from 'react';
 import styles from './App.module.css';
 import Title from './components/Title/Title';
+import Registration from './components/Registration/Registration';
 
 function App(): JSX.Element {
   return (
     <main className={styles.container}>
       <Title text="Bergfest" />
-      <form className={styles.form}>
-        <span className={styles.options}>select participant</span>
-        <select className={styles.selection}>
-          <option>Leon Machens</option>
-          <option>Lara Janzen</option>
-        </select>
-        <span className={styles.options}>or create new</span>
-        <input
-          type="text"
-          className={styles.inputField}
-          placeholder="first name"
-        ></input>
-        <input
-          type="text"
-          className={styles.inputField}
-          placeholder="last name"
-        ></input>
-        <input type="submit" className={styles.button} value="Enter"></input>
-      </form>
+      <Registration />
     </main>
   );
 }
---
+++ src/app/components/Registration/Registration.tsx
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import styles from './Registration.module.css';
+
+function Registration(): JSX.Element {
+  //   const [firstName, setFirstName] = useState('');
+  return (
+    <form className={styles.form}>
+      <span className={styles.options}>select participant</span>
+      <select className={styles.selection}>
+        <option>Leon Machens</option>
+        <option>Lara Janzen</option>
+      </select>
+      <span className={styles.options}>or create new</span>
+      <input
+        type="text"
+        className={styles.inputField}
+        placeholder="first name"
+      ></input>
+      <input
+        type="text"
+        className={styles.inputField}
+        placeholder="last name"
+      ></input>
+      <input type="submit" className={styles.button} value="Enter"></input>
+    </form>
+  );
+}
+
+export default Registration;
