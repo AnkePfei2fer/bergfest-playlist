7584be394f8f379e4aad1d3e5f01b9a033ca0e8e
Anke <pfeiffer.anke@outlook.com>
2021-11-03 17:11:48 +0100

    :sparkles: Control last name

--- src/app/components/Registration/Registration.tsx
+++ src/app/components/Registration/Registration.tsx
@@ -3,7 +3,9 @@ import styles from './Registration.module.css';
 
 function Registration(): JSX.Element {
   const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   console.log(firstName);
+  console.log(lastName);
   return (
     <form className={styles.form}>
       <span className={styles.options}>select participant</span>
@@ -23,6 +25,8 @@ function Registration(): JSX.Element {
         type="text"
         className={styles.inputField}
         placeholder="last name"
+        value={lastName}
+        onChange={(event) => setLastName(event.target.value)}
       ></input>
       <input type="submit" className={styles.button} value="Enter"></input>
     </form>
