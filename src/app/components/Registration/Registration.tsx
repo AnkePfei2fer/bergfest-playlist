d18deb7b440b59b275529676117d80e1f7f170b2
Anke <pfeiffer.anke@outlook.com>
2021-11-04 10:02:46 +0100

    :recycle: Add handleSubmit function

--- src/app/components/Registration/Registration.tsx
+++ src/app/components/Registration/Registration.tsx
@@ -1,13 +1,22 @@
-import React, { useState } from 'react';
+import React, { ChangeEvent, FormEvent, useState } from 'react';
 import styles from './Registration.module.css';
 
 function Registration(): JSX.Element {
   const [firstName, setFirstName] = useState('');
   const [lastName, setLastName] = useState('');
-  console.log(firstName);
-  console.log(lastName);
+  function handleSubmit(event: FormEvent) {
+    event.preventDefault();
+    console.log(firstName, lastName);
+  }
+  function handleFirstNameChange(event: ChangeEvent<HTMLInputElement>) {
+    setFirstName(event.target.value);
+  }
+  function handleLastNameChange(event: ChangeEvent<HTMLInputElement>) {
+    setLastName(event.target.value);
+  }
+
   return (
-    <form className={styles.form}>
+    <form className={styles.form} onSubmit={handleSubmit}>
       <span className={styles.options}>select participant</span>
       <select className={styles.selection}>
         <option>Leon Machens</option>
@@ -19,14 +28,14 @@ function Registration(): JSX.Element {
         className={styles.inputField}
         placeholder="first name"
         value={firstName}
-        onChange={(event) => setFirstName(event.target.value)}
+        onChange={handleFirstNameChange}
       ></input>
       <input
         type="text"
         className={styles.inputField}
         placeholder="last name"
         value={lastName}
-        onChange={(event) => setLastName(event.target.value)}
+        onChange={handleLastNameChange}
       ></input>
       <input type="submit" className={styles.button} value="Enter"></input>
     </form>
